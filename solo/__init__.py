c8685207da4c67b371d80df0b10c97aa351c7ab9
Lazybird <go@lazybird.co>
2016-02-07 12:22:49 +0100

    Update version and change log

--- solo/__init__.py
+++ solo/__init__.py
@@ -1,6 +1,6 @@
 """django-solo helps working with singletons: things like global settings that you want to edit from the admin site.
 """
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 default_app_config = 'solo.apps.SoloAppConfig'
