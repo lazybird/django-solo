5309c5a89136bcb1f857e77c3b9c1c2449930e85
Sylvain <sylvain.toe@myco.coop>
2018-01-15 17:12:14 +0100

    Update notes and version for 1.1.3

--- solo/__init__.py
+++ solo/__init__.py
@@ -1,6 +1,6 @@
 """django-solo helps working with singletons: things like global settings that you want to edit from the admin site.
 """
 
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 
 default_app_config = 'solo.apps.SoloAppConfig'
