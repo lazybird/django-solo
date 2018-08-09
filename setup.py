4414d11f2e53ac40499b1e090673d321d231b077
charlesthk <charles@vingtcinq.io>
2018-08-09 15:42:15 +0200

    add README.rst for PyPi

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@ from setuptools import setup, find_packages
 
 import solo
 
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
+
+# When running tests using tox, README.md is not found
+try:
+    with open(README) as file:
+        long_description = file.read()
+except Exception:
+    long_description = ''
 
 setup(
     name='django-solo',
     version=solo.__version__,
-    description=solo.__doc__,
+    description=long_description,
     packages=find_packages(),
     url='http://github.com/lazybird/django-solo/',
     author='lazybird',
