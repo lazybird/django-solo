99c5d366025f6a133712455bb009d79c4efb129c
charlesthk <charles@vingtcinq.io>
2018-08-09 21:46:54 +0200

    update change_form for newer versions of Django

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ except Exception:
 setup(
     name='django-solo',
     version=solo.__version__,
-    description=long_description,
+    description=solo.__doc__,
     packages=find_packages(),
     url='http://github.com/lazybird/django-solo/',
     author='lazybird',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     include_package_data=True,
     license='Creative Commons Attribution 3.0 Unported',
 )
