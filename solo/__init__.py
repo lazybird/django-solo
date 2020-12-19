50ce603a74a680a13a8c18c98c2aace160c7a98d
Sylvain <lazybird@users.noreply.github.com>
2020-12-19 06:30:29 +0100

    Add release notes and set version to 1.1.4

--- solo/__init__.py
+++ solo/__init__.py
@@ -1,7 +1,7 @@
 """django-solo helps working with singletons: things like global settings that you want to edit from the admin site.
 """
 
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 __doc__ = 'Django Solo helps working with singletons'
 
 default_app_config = 'solo.apps.SoloAppConfig'
