091f7194e47705388b26be43803941eb11aa6f23
johnthagen <johnthagen@gmail.com>
2023-12-28 10:18:08 -0500

    Drop support for end of life Django 4.0 and 4.1

--- setup.py
+++ setup.py
@@ -40,8 +40,6 @@ setup(
     license='Creative Commons Attribution 3.0 Unported',
     classifiers=[
         'Framework :: Django :: 3.2',
-        'Framework :: Django :: 4.0',
-        'Framework :: Django :: 4.1',
         'Framework :: Django :: 4.2',
         'Framework :: Django :: 5.0',
         'Intended Audience :: Developers',
