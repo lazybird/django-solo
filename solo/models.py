797613442b3fad3404645768d7783ff64b2be6ad
Viicos <65306057+Viicos@users.noreply.github.com>
2024-01-15 10:11:49 +0100

    Fix version

--- solo/models.py
+++ solo/models.py
@@ -22,7 +22,7 @@ DEFAULT_SINGLETON_INSTANCE_ID = 1
 def get_cache(cache_name: str) -> BaseCache:
     warnings.warn(
         DeprecationWarning,
-        "'get_cache' is deprecated and will be removed in django-solo 2.3.0. "
+        "'get_cache' is deprecated and will be removed in django-solo 2.4.0. "
         "Instead, use 'caches' from 'django.core.cache'.",
         stacklevel=2,
     )
