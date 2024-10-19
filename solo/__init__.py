d330885536135ce16af4f4df73250de1ab4e830b
johnthagen <johnthagen@users.noreply.github.com>
2024-10-19 09:01:08 -0400

    Simplify default_app_config after dropping support for Django <3.2 (#143)

--- solo/__init__.py
+++ solo/__init__.py
@@ -3,9 +3,4 @@ django-solo helps working with singletons:
 things like global settings that you want to edit from the admin site.
 """
 
-import django
-
 __version__ = "2.3.0"
-
-if django.VERSION < (3, 2):
-    default_app_config = "solo.apps.SoloAppConfig"
